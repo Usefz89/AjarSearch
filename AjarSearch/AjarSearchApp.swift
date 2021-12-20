a509b8a2083616725d3e9c9640bb8df88d8cb828
Yosuef zuriqi <usefz89@gmail.com>
2021-12-20 15:25:53 +0300

    Initial Commit

---
+++ AjarSearch/AjarSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AjarSearchApp.swift
+//  AjarSearch
+//
+//  Created by Yousef Zuriqi on 20/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct AjarSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
